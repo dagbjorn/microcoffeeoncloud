0e87edc2b97e1a64fca8447385d623ea1da5a973
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2018-02-24 16:32:45 +0100

    Eliminated elaborate filtering function of environment variables. No longer needed with API gateway. The address of consumed REST services can be found from current web page.

--- microcoffeeoncloud-web/src/main/resources/static/js/env.js
+++ microcoffeeoncloud-web/src/main/resources/static/js/env.js
@@ -7,7 +7,7 @@
     window.__env = window.__env || {};
 
     // REST API gateway
-    window.__env.apiGatewayUrl = '${app.gateway.url}';
+    window.__env.apiGatewayUrl = window.location.href.split('/').slice(0, 3).join('/');
 
     // Logging
     window.__env.enableDebug = true;
