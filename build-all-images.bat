2b928cfba6f1dad950a70e9669c8d473578b31ba
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2018-02-25 14:46:23 +0100

    Migrated from retired maven-eclipse-plugin to m2e.

--- build-all-images.bat
+++ build-all-images.bat
@@ -1,8 +1,8 @@
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-configserver\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-database\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-discovery\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-gateway\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-creditrating\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-location\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-order\pom.xml
-call mvn clean package docker:build eclipse:eclipse -f microcoffeeoncloud-web\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-configserver\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-database\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-discovery\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-gateway\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-creditrating\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-location\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-order\pom.xml
+call mvn clean package docker:build -f microcoffeeoncloud-web\pom.xml
