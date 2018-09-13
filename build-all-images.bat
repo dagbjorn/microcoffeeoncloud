e3ff8506e5841679a4d389d5f0ccf9fea8c3aab3
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2018-09-13 20:53:40 +0200

    Added push-image profile to push image (tags: latest and project.version) to DockerHub. Changed docker-compose files to use dagbjorn-prefixed images.

--- build-all-images.bat
+++ build-all-images.bat
@@ -1,7 +1,10 @@
-call mvn clean package docker:build -f microcoffeeoncloud-configserver\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-database\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-discovery\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-gateway\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-creditrating\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-location\pom.xml
-call mvn clean package docker:build -f microcoffeeoncloud-order\pom.xml
+::
+:: build-all-images [-Ppush-image]
+::
+call mvn clean package -f microcoffeeoncloud-configserver\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-database\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-discovery\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-gateway\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-creditrating\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-location\pom.xml %*
+call mvn clean package -f microcoffeeoncloud-order\pom.xml %*
