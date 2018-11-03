4d89ef808eb94b269cea7298be3382aaea447c21
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2018-11-03 09:51:57 +0100

    Improved batch file to abort on build failure.

--- build-all-images.bat
+++ build-all-images.bat
@@ -1,10 +1,15 @@
 ::
-:: build-all-images [-Ppush-image]
+:: build-all-images [-Pbuild-image] [-Pbuild-image,push-image] [-Prun-itest]
 ::
-call mvn clean package -f microcoffeeoncloud-configserver\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-database\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-discovery\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-gateway\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-creditrating\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-location\pom.xml %*
-call mvn clean package -f microcoffeeoncloud-order\pom.xml %*
+call mvn clean install -f microcoffeeoncloud-configserver\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-database\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-discovery\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-gateway\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-creditrating\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-location\pom.xml %* || goto :error
+call mvn clean install -f microcoffeeoncloud-order\pom.xml %* || goto :error
+goto :EOF
+
+:error
+echo FAILED WITH ERROR #%errorlevel%.
+exit /b %errorlevel%
