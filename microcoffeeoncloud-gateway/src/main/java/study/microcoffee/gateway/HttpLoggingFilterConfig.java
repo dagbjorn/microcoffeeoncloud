8ff9e2a051d5b490b76d97fde2df1e7de0e52f6d
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2018-11-12 19:46:39 +0100

    Upgraded mongo image 4.0.3 -> 4.0.4. Added HTTP logging in Gateway.

---
+++ microcoffeeoncloud-gateway/src/main/java/study/microcoffee/gateway/HttpLoggingFilterConfig.java
@@ -0,0 +1,40 @@
+package study.microcoffee.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+import study.microcoffee.gateway.logging.HttpLoggingFilter;
+
+/**
+ * Configuration class of HTTP logging filter.
+ * <p>
+ * Configure the logging filter as follows:
+ *
+ * <pre>
+ * logging.level.study.microcoffee.gateway.logging.HttpLoggingFilter = DEBUG
+ * </pre>
+ */
+@Configuration
+public class HttpLoggingFilterConfig {
+
+    @Value("${formatted.logging:false}")
+    private boolean formattedLogging;
+
+    @Bean
+    public FilterRegistrationBean<HttpLoggingFilter> requestLoggingFilterRegistrationBean() {
+        FilterRegistrationBean<HttpLoggingFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(createRequestLogFilter());
+        registrationBean.addUrlPatterns("/api/*");
+        registrationBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registrationBean;
+    }
+
+    private HttpLoggingFilter createRequestLogFilter() {
+        HttpLoggingFilter filter = new HttpLoggingFilter();
+        filter.setFormattedLogging(formattedLogging);
+        return filter;
+    }
+}
