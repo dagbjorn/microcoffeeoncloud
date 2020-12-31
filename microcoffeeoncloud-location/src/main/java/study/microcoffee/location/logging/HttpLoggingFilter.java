d1a336772bb1667a80f6013b38579132b844ac1f
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2020-12-31 09:48:31 +0100

    Fixed SonarLint issues.

--- microcoffeeoncloud-location/src/main/java/study/microcoffee/location/logging/HttpLoggingFilter.java
+++ microcoffeeoncloud-location/src/main/java/study/microcoffee/location/logging/HttpLoggingFilter.java
@@ -219,10 +219,9 @@ public class HttpLoggingFilter extends OncePerRequestFilter {
 
         for (Entry<String, List<String>> header : headers.entrySet()) {
             String headerName = header.getKey();
+            List<String> values = header.getValue();
 
             builder.append(headerName).append(": ");
-
-            List<String> values = headers.get(headerName);
 
             for (int i = 0; i < values.size(); i++) {
                 if (i > 0) {
