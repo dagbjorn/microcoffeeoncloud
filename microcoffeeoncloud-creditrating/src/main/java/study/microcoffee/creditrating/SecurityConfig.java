d76a694d7aef6578c178e03e8974a349184bc4cd
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2021-05-26 18:27:50 +0200

    Simplied HttpSecurity config.

--- microcoffeeoncloud-creditrating/src/main/java/study/microcoffee/creditrating/SecurityConfig.java
+++ microcoffeeoncloud-creditrating/src/main/java/study/microcoffee/creditrating/SecurityConfig.java
@@ -11,8 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests() //
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity //
+            .authorizeRequests() //
             .mvcMatchers("/api/**").hasAuthority("SCOPE_creditrating") //
             .and() //
             .oauth2ResourceServer() //
--- microcoffeeoncloud-order/src/main/java/study/microcoffee/order/SecurityConfig.java
+++ microcoffeeoncloud-order/src/main/java/study/microcoffee/order/SecurityConfig.java
@@ -1,7 +1,6 @@
 package study.microcoffee.order;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,16 +12,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity //
             .csrf().disable() //
             .authorizeRequests() //
-            .antMatchers(HttpMethod.GET, "/api/coffeeshop/menu").permitAll() //
-            .antMatchers(HttpMethod.POST, "/api/coffeeshop/**/order").permitAll() //
-            .antMatchers(HttpMethod.GET, "/api/coffeeshop/**/order/**").permitAll() //
-            .antMatchers(HttpMethod.GET, "/apidocs/order/v2/api-docs").permitAll() //
-            .antMatchers(HttpMethod.GET, "/internal/isalive").permitAll() //
-            .antMatchers(HttpMethod.GET, "/internal/isready").permitAll() //
-            .antMatchers(HttpMethod.GET, "/actuator/**").permitAll() //
-            .anyRequest().authenticated() //
-            //.and() //
-            //.oauth2Client() // Only needed for the authorization code grant flow which we don't use.
-            ;
+            .antMatchers("/**").permitAll() //
+        // .and() //
+        // .oauth2Client() // Only needed for the authorization code grant flow which we don't use.
+        ;
     }
 }
