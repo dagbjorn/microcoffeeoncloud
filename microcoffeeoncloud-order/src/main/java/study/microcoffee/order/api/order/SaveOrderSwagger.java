987527b2d12606036dead5ffb98b80a970eabd84
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2021-12-26 09:23:28 +0100

    Implemented Swagger annotation class of Menu and Order APIs.

---
+++ microcoffeeoncloud-order/src/main/java/study/microcoffee/order/api/order/SaveOrderSwagger.java
@@ -0,0 +1,34 @@
+package study.microcoffee.order.api.order;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import study.microcoffee.order.SwaggerConfig;
+
+/**
+ * API documentation of operation to save an order.
+ */
+@Documented
+@Retention(RUNTIME)
+@Target(METHOD)
+@Operation(operationId = "saveOrder", //
+    tags = { SwaggerConfig.ORDER_TAG }, //
+    summary = "Creates an order.", //
+    parameters = { //
+        @Parameter(in = ParameterIn.PATH, name = "coffeeShopId", description = "Coffee shop ID.", example = "1"), //
+        @Parameter(ref = SwaggerConfig.CORRELATION_ID_HEADER) }, //
+    responses = { //
+        @ApiResponse(responseCode = "201", description = "New order created."), //
+        @ApiResponse(responseCode = "402", description = "Too low credit rating to accept order. Payment required!", content = @Content), //
+        @ApiResponse(responseCode = "500", description = "Internal server error.", content = @Content) })
+public @interface SaveOrderSwagger {
+}
