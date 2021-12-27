67f835ee6c232f57edce60a53c986918d7244f7d
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2021-12-27 13:31:27 +0100

    Added JSON example of a coffee order.

--- microcoffeeoncloud-order/src/main/java/study/microcoffee/order/api/order/CreateOrderSwagger.java
+++ microcoffeeoncloud-order/src/main/java/study/microcoffee/order/api/order/CreateOrderSwagger.java
@@ -11,7 +11,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import study.microcoffee.order.SwaggerConfig;
 
@@ -27,6 +29,9 @@ import study.microcoffee.order.SwaggerConfig;
     parameters = { //
         @Parameter(in = ParameterIn.PATH, name = "coffeeShopId", description = "Coffee shop ID.", schema = @Schema(type = "string", example = "1")), //
         @Parameter(ref = SwaggerConfig.CORRELATION_ID_HEADER) }, //
+    requestBody = @RequestBody(description = "JSON-formatted coffee order.", //
+        content = @Content(examples = {
+            @ExampleObject(name = "Coffee order with options", value = "{\"coffeeShopId\":1,\"drinker\":\"Dagbjørn\",\"size\":\"Small\",\"type\":{\"name\":\"Americano\",\"family\":\"Coffee\"},\"selectedOptions\":[\"soy\",\"decaf\"]}") })), //
     responses = { //
         @ApiResponse(responseCode = "201", description = "New order created."), //
         @ApiResponse(responseCode = "402", description = "Too low credit rating to accept order. Payment required!", content = @Content) })
