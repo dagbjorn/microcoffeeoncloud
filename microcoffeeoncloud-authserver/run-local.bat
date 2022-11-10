f928b4d89a6e86cc491a158e65bd284441375de2
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2022-11-10 19:55:05 +0100

    Updated Kubernetes manifests, deploy script and deploy-gke workflow to support Keycloak Quarkus.

--- microcoffeeoncloud-authserver/run-local.bat
+++ microcoffeeoncloud-authserver/run-local.bat
@@ -13,6 +13,6 @@ md %KEYCLOAK_HOME%\data\import\
 copy src\main\keycloak\microcoffee-realm.json %KEYCLOAK_HOME%\data\import\.
 
 cd /d %KEYCLOAK_HOME%
-bin\kc.bat start-dev --import-realm --http-enabled=true --http-port=8093 --https-port=8456 --https-key-store-file=localhost.p12 --https-key-store-password=12345678
+bin\kc.bat start-dev --import-realm --http-enabled=true --http-port=8093 --https-port=8456 --https-key-store-file=localhost.p12 --https-key-store-password=12345678 --health-enabled=true
 
 endlocal
