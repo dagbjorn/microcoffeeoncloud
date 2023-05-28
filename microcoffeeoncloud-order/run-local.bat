f9d675a8a7df01cd4917a2492de88325557a0d9b
Dagbjørn Nogva <dagbjorn@users.noreply.github.com>
2023-05-28 09:54:19 +0200

    Springboot 310 upgrade (#87)
    * Upgraded Spring Boot 3.0.6 -> 3.1.0 (order: 2.7.11 -> 2.7.12), Keycloak Docker image 20.0.1 -> 21.1.1 and latest Maven plugins.
    
    Fixed deprecations in HttpSecurity configuration.
    
    lifecycle-mapping workaround for issue with Spring 3.1.0 and maven-dependency-plugin 3.4.0 and later.
    
    Change unpacking of certificates artifact to process-resources so that it is run when using spring-boot:run.
    
    * Added lifecycle-mapping plugin to workaround maven-dependency-plugin issue with m2eclipse.
    
    * Upgraded Spring Cloud 2022.0.2 -> 2022.0.3, mongo 6.0.5 -> 6.0.6 and gmavenplus-plugin 2.1.0 -> 3.0.0.

--- microcoffeeoncloud-order/run-local.bat
+++ microcoffeeoncloud-order/run-local.bat
@@ -4,7 +4,7 @@ setlocal
 
 set ACTIVE_SPRING_PROFILES=devlocal
 set SPRING_CLOUD_CONFIG_URI=https://localhost:8454
-set ORDER_CLIENT_SECRET=RnsJyjxIadMdjlsS6scJlBA9rD9OdYQP
+set ORDER_CLIENT_SECRET=YA1RLrWp6ukuustl1hu4akClv6qYi9C3
 
 mvn spring-boot:run -Dspring-boot.run.profiles=%ACTIVE_SPRING_PROFILES%
 
