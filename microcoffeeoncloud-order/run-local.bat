ca01118871bd2632dd4977006326762d05092964
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2023-10-02 20:01:17 +0200

    Updated ORDER_CLIENT_SECRET.

--- microcoffeeoncloud-order/run-local.bat
+++ microcoffeeoncloud-order/run-local.bat
@@ -4,7 +4,7 @@ setlocal
 
 set ACTIVE_SPRING_PROFILES=devlocal
 set SPRING_CLOUD_CONFIG_URI=https://localhost:8454
-set ORDER_CLIENT_SECRET=MQQzBkz0KhvUInTU7cdYgqrrAD0fdgfS
+set ORDER_CLIENT_SECRET=x6UeVnSOeu7wB8ocM5SNEJPEyjA924Vi
 
 mvn spring-boot:run -Dspring-boot.run.profiles=%ACTIVE_SPRING_PROFILES%
 
