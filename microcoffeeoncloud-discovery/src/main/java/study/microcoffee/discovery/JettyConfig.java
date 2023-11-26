61904223f736703020dbe8dd4c8c9bd9566f84b8
Dagbjørn Nogva <dagbjorn@users.noreply.github.com>
2023-11-26 18:09:27 +0100

    Switched back to Jetty from Tomcat after upgrade to Spring Boot 3.2.0. (#161)

---
+++ microcoffeeoncloud-discovery/src/main/java/study/microcoffee/discovery/JettyConfig.java
@@ -0,0 +1,42 @@
+package study.microcoffee.discovery;
+
+import org.eclipse.jetty.server.NetworkTrafficServerConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ * Additional Jetty configuration not supported by Spring Boot.
+ */
+@Configuration
+@Profile("!itest")
+public class JettyConfig {
+
+    private final Logger logger = LoggerFactory.getLogger(JettyConfig.class);
+
+    /**
+     * Adds an additional network connector for use by a http port that comes in addition to the https port configured by the
+     * standard Spring Boot properties in <code>application.properties</code>.
+     *
+     * @param httpPort
+     *            the http port configured by the <code>server.http.port</code> property in <code>application.properties</code>.
+     * @return The JettyServletWebServerFactory object.
+     */
+    @Bean
+    public JettyServletWebServerFactory jettyServletWebServerFactory(@Value("${server.http.port}") final String httpPort) {
+        logger.info("Adding network connector of port {}", httpPort);
+
+        JettyServletWebServerFactory factory = new JettyServletWebServerFactory();
+        factory.addServerCustomizers(server -> {
+            NetworkTrafficServerConnector connector = new NetworkTrafficServerConnector(server);
+            connector.setPort(Integer.valueOf(httpPort));
+            server.addConnector(connector);
+        });
+
+        return factory;
+    }
+}
