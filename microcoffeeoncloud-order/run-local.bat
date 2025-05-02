d438a84e7ae37cae2d4aa02e1b0e21d620ffd57e
Dagbjørn Nogva <dagbjorn@users.noreply.github.com>
2025-05-02 17:58:11 +0200

    Update deps 202502 (#375)
    * Updated keycloak 26.2.1 -> 26.2.2, mongo 8.0.8 -> 8.0.9, modelmapper 3.2.2 -> 3.2.3 and latest plugins.
    
    * Frontend update: react-router-dom 7.5.2 -> 7.5.3.

--- microcoffeeoncloud-order/run-local.bat
+++ microcoffeeoncloud-order/run-local.bat
@@ -4,7 +4,7 @@ setlocal
 
 set ACTIVE_SPRING_PROFILES=devlocal
 set SPRING_CLOUD_CONFIG_URI=https://localhost:8454
-set ORDER_CLIENT_SECRET=G6NB2uEqyE6dWqVqV7yI8HpA5LeBxFIi
+set ORDER_CLIENT_SECRET=amJyTUVAoViUuJc7a7EaqlVo3OWHyimy
 
 mvn spring-boot:run -Dspring-boot.run.profiles=%ACTIVE_SPRING_PROFILES%
 
