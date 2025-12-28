73366100eb18322afc4de72de6a5a232b9b62a84
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2025-12-28 09:10:54 +0100

    Implemented workaround in DataMongoTest-annotated test to start and stop EmbeddedMongoDB manually.

--- microcoffeeoncloud-order/src/test/java/study/microcoffee/order/repository/EmbeddedMongoOrderRepositoryIT.java
+++ microcoffeeoncloud-order/src/test/java/study/microcoffee/order/repository/EmbeddedMongoOrderRepositoryIT.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
 
-import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.mongodb.test.autoconfigure.DataMongoTest;
@@ -12,6 +13,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.repository.Repository;
 import org.springframework.test.context.TestPropertySource;
 
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.mongo.transitions.Mongod;
+import de.flapdoodle.embed.mongo.transitions.RunningMongodProcess;
+import de.flapdoodle.reverse.TransitionWalker;
+import de.flapdoodle.reverse.transitions.Start;
 import study.microcoffee.order.domain.DrinkType;
 import study.microcoffee.order.domain.Order;
 import study.microcoffee.order.test.DiscoveryTestConfig;
@@ -20,15 +27,41 @@ import study.microcoffee.order.test.DiscoveryTestConfig;
  * Integration tests of {@link OrderRepository} that uses an auto-configured Embedded MongoDB database.
  * <p>
  * The @DataMongoTest annotation will scan for @Document classes and Spring {@link Repository} classes.
+ *
+ * @implNote DataMongoTest doesn't autoconfigure and start Embedded MongoDB with Spring Boot 4 and
+ *           de.flapdoodle.embed.mongo.spring4x, hence we start MongoDB manually running on a fixed port.
  */
-@Disabled("Doesn't autoconfigure and start Embedded MongoDB with Spring Boot 4 and de.flapdoodle.embed.mongo.spring4x")
-@DataMongoTest
+@DataMongoTest(properties = {
+    "spring.mongodb.uri=mongodb://localhost:" + EmbeddedMongoOrderRepositoryIT.MONGODB_PORT + "/microcoffee?ssl=false" })
 @TestPropertySource("/application-test.properties")
 @Import(DiscoveryTestConfig.class)
 class EmbeddedMongoOrderRepositoryIT {
 
+    static final int MONGODB_PORT = 28017;
+    static final Version.Main MONGODB_VERSION = Version.Main.V8_2;
+
+    private static TransitionWalker.ReachedState<RunningMongodProcess> running;
+
     @Autowired
     private OrderRepository orderRepository;
+
+    // Ref. https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo/blob/main/docs/Howto.md
+    @BeforeAll
+    static void startMongodb() {
+        Mongod mongod = Mongod.builder() //
+            .net(Start.to(Net.class).initializedWith(Net.defaults().withPort(MONGODB_PORT))) //
+            .build();
+
+        running = mongod.start(MONGODB_VERSION);
+    }
+
+    @AfterAll
+    static void teardownMongodb() {
+        if (running != null) {
+            running.close();
+        }
+        running = null;
+    }
 
     @Test
     void saveOrderWhenReadBackShouldReturnSavedOrder() {
