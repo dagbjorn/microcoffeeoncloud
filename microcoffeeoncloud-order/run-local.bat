76d8869edbbcd887e0ad24629f0700e6950971ef
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2026-01-01 16:52:17 +0100

    New token for run-local.

--- microcoffeeoncloud-order/run-local.bat
+++ microcoffeeoncloud-order/run-local.bat
@@ -4,7 +4,7 @@ setlocal
 
 set ACTIVE_SPRING_PROFILES=devlocal
 set SPRING_CLOUD_CONFIG_URI=https://localhost:8454
-set ORDER_CLIENT_SECRET=xNglsWLsI221p1gam0vCoH5aHQdVJGgp
+set ORDER_CLIENT_SECRET=KT3ivBIRCmeKO2ZWiYVXSQXAlx6jW9Gn
 
 mvn spring-boot:run -Dspring-boot.run.profiles=%ACTIVE_SPRING_PROFILES%
 
