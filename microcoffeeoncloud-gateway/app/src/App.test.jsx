2c5edd025427c0b06191602e20252777a0f4d0df
Dagbjørn Nogva <dagbjorn@users.noreply.github.com>
2026-04-03 11:26:38 +0200

    Vite migration manual (by me) (#531)
    * Migrated from Create React App (CRA) to Vite.
    
    * Some changes picked up from Clude Haikus test migration.

--- microcoffeeoncloud-gateway/app/src/App.test.jsx
+++ microcoffeeoncloud-gateway/app/src/App.test.jsx
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import { CookiesProvider } from 'react-cookie';
 import App from './App';
+import { test } from 'vitest';
 
 test('renders App', () => {
-  render(<App />);
+  render(
+    <CookiesProvider>
+      <App />
+    </CookiesProvider>
+  );
   const linkElement = screen.getByText(/Microcoffee/i);
   expect(linkElement).toBeInTheDocument();
 });
