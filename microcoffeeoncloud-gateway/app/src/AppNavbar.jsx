b63f6f91a512f36fc28756b60a89020f299b3c79
Dagbjorn Nogva <dagbjorn.nogva@broadpark.no>
2026-04-04 18:09:39 +0200

    Reduced left and right margins of links in AppNavBar.

--- microcoffeeoncloud-gateway/app/src/AppNavbar.jsx
+++ microcoffeeoncloud-gateway/app/src/AppNavbar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const AppNavbar = () => {
     return (
         <Navbar bg="dark" variant="dark" expand="md">
-            <Container>
+            <Container fluid className="px-4">
                 <Navbar.Brand as={Link} to="/">Microcoffee</Navbar.Brand>
                 <Navbar.Toggle />
                 <Navbar.Collapse className="justify-content-end">
